fc4366bb5dbe1da4bb17acd26227321a8c7a881c
Nivedhan Senthilkumar <nivedhan1998@gmail.com>
2022-09-25 00:51:11 +0100

    Update News Classification.py

--- News Classification.py
+++ News Classification.py
@@ -1,9 +1,11 @@
 
-
-import nltk #Importing NLP library
+#LIBRARIES
+import nltk
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-messages = pd.read_csv("./data/train.csv",nrows=60000)
+#DATA IMPORT
+messages = pd.read_csv("C:/Users/nivedhan/Downloads/Natural-Language-Processing-main/Natural-Language-Processing-main/News Classification/data/train.csv",nrows=60000)
+messages.head(5)
